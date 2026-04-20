bc36a3422f46cfb91e169c81a37a6f688680bcdc
Mathias <mathias.yde@hotmail.dk>
2026-04-21 00:55:05 +0200

    add task to copy apk file to target folder

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,15 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.gradle.api.tasks.Copy
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+}
+
+tasks.register<Copy>("copyApkToTarget") {
+    dependsOn(":app:assembleDebug")
+
+    from(project(":app").layout.buildDirectory.file("outputs/apk/debug/app-debug.apk"))
+    into(layout.projectDirectory.dir("target/"))
+    rename { "app.apk" }
 }
