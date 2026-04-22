a555a5d95fefcaccc43e2548f21eafdd8138a06d
Mathias <mathias.yde@hotmail.dk>
2026-04-22 13:03:21 +0200

    update vue app

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,36 +1,65 @@
 <template>
   <main>
     <h1>Android Hybrid</h1>
-    <p>This webpage demonstrates a WebView's ability to interact with native Android features.</p>
+
+    <section>
+      <!-- <p>This webpage demonstrates the WebView's ability to interact with native Android features.</p>
+       <p>This demonstration shows a webpage's ability to interface with native Android features through a WebView.</p>
+       <p>This webpage is supposed to run on the dedicated Android app which has the necessary JavaScriptInterface bridge.</p> -->
+
+      <p>This is a demonstration of WebView's ability to interact with native Android features. Through an JavaScriptInterface, the webpage can access native Android functionality.</p>
+      <p>For this showcase, the page requests the app to take a photo using the device's camera, and display the taken photo on the webpage.
+      Successfully demonstrating back-to-back communication through the WebView and JavaScriptInterface.</p>
+    </section>
+
 
     <p id="bridge-status">{{ window.native ? "✅ Android bridge detected" : "❌ No Android bridge detected" }}</p>
 
-    <p>If the Android bridge is detected, you can continue to request the neccessary permissions for this demonstration.</p>
+    <section v-if="!window.native">
+      <p>It doesn't seem like you're running this webpage inside the Android app.</p>
+      <p>(No window.native object detected)</p>
+    </section>
 
-    <button @click="requestPermissions">
-      Request permissions
-    </button>
+    <section v-if="window.native">
+      <p>Please click the button below to request the necessary permissions for this demonstration.</p>
 
-    <table>
-      <thead>
-        <tr>
-          <th>Permission</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(value, key) in permissions" :key="key">
-          <td>{{ key }}</td>
-          <td>{{ value ? "✅" : "❌" }}</td>
-        </tr>
-      </tbody>
-    </table>
+      <button @click="requestPermissions">
+        Request permissions
+      </button>
 
-    <button @click="takePhoto">
-      Take Photo
-    </button>
+      <p>The table shows what permissions are available.</p>
 
-    <img v-if="photo" :src="`data:image/jpeg;base64,${photo}`" alt="Taken Photo">
+      <table>
+        <thead>
+          <tr>
+            <th>Permission</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(value, key) in permissions" :key="key">
+            <td>{{ key }}</td>
+            <td>{{ value ? "✅" : "❌" }}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p>If you have granted the camera permission, you can now take a photo.</p>
+      <p>Per default, Android emulators simulate a camera with a fake video feed.</p>
+
+      <button @click="takePhoto">
+        Take Photo
+      </button>
+
+      <img v-if="photo" :src="`data:image/jpeg;base64,${photo}`" alt="Taken Photo">
+
+      <p>
+        If you are running this demonstration with FRIDA instrumentation with the sentry hooks,
+        you should see the logged permission requests and camera access in the console output of the Android app.
+
+        The Android JavaScriptInterface is detected and all of its methods are hooked too, allowing you to see the communication between the webpage and the native Android app in real time.
+      </p>
+    </section>
   </main>
 
 </template>
@@ -85,6 +114,13 @@ main {
   flex-direction: column;
   align-items: center;
   gap: 0.4em;
+}
+
+section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1em;
 }
 
 main > h1 {
