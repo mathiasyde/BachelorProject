da05fe7e3fdfc6ffc3207f1a6b96ec720befd30d
Mathias <mathias.yde@hotmail.dk>
2026-05-09 20:00:00 +0200

    Update App.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,29 +3,41 @@
     <h1>Android Hybrid</h1>
 
     <section>
-      <!-- <p>This webpage demonstrates the WebView's ability to interact with native Android features.</p>
-       <p>This demonstration shows a webpage's ability to interface with native Android features through a WebView.</p>
-       <p>This webpage is supposed to run on the dedicated Android app which has the necessary JavaScriptInterface bridge.</p> -->
-
-      <p>This is a demonstration of WebView's ability to interact with native Android features. Through an JavaScriptInterface, the webpage can access native Android functionality.</p>
-      <p>For this showcase, the page requests the app to take a photo using the device's camera, and display the taken photo on the webpage.
-      Successfully demonstrating back-to-back communication through the WebView and JavaScriptInterface.</p>
+      <p>
+        This is a demonstration of WebView's ability to interact with native
+        Android features. Through an JavaScriptInterface, the webpage can access
+        native Android functionality.
+      </p>
+      <p>
+        For this showcase, the page requests the app to take a photo using the
+        device's camera, and display the taken photo on the webpage.
+        Successfully demonstrating back-to-back communication through the
+        WebView and JavaScriptInterface.
+      </p>
     </section>
 
-
-    <p id="bridge-status">{{ window.native ? "✅ Android bridge detected" : "❌ No Android bridge detected" }}</p>
+    <p id="bridge-status">
+      {{
+        window.native
+          ? "✅ Android bridge detected"
+          : "❌ No Android bridge detected"
+      }}
+    </p>
 
     <section v-if="!window.native">
-      <p>It doesn't seem like you're running this webpage inside the Android app.</p>
+      <p>
+        It doesn't seem like you're running this webpage inside the Android app.
+      </p>
       <p>(No window.native object detected)</p>
     </section>
 
     <section v-if="window.native">
-      <p>Please click the button below to request the necessary permissions for this demonstration.</p>
+      <p>
+        Please click the button below to request the necessary permissions for
+        this demonstration.
+      </p>
 
-      <button @click="requestPermissions">
-        Request permissions
-      </button>
+      <button @click="requestPermissions">Request permissions</button>
 
       <p>The table shows what permissions are available.</p>
 
@@ -44,76 +56,86 @@
         </tbody>
       </table>
 
-      <p>If you have granted the camera permission, you can now take a photo.</p>
-      <p>Per default, Android emulators simulate a camera with a fake video feed.</p>
+      <p>
+        If you have granted the camera permission, you can now take a photo.
+      </p>
+      <p>
+        Per default, Android emulators simulate a camera with a fake video feed.
+      </p>
 
-      <button @click="takePhoto">
-        Take Photo
-      </button>
+      <button @click="takePhoto">Take Photo</button>
 
-      <img v-if="photo" :src="`data:image/jpeg;base64,${photo}`" alt="Taken Photo">
+      <img
+        v-if="photo"
+        :src="`data:image/jpeg;base64,${photo}`"
+        alt="Taken Photo"
+      />
 
       <p>
-        If you are running this demonstration with FRIDA instrumentation with the sentry hooks,
-        you should see the logged permission requests and camera access in the console output of the Android app.
-
-        The Android JavaScriptInterface is detected and all of its methods are hooked too, allowing you to see the communication between the webpage and the native Android app in real time.
+        If you are running this demonstration with FRIDA instrumentation with
+        the sentry hooks, you should see the logged permission requests and
+        camera access in the console output of the Android app. The Android
+        JavaScriptInterface is detected and all of its methods are hooked too,
+        allowing you to see the communication between the webpage and the native
+        Android app in real time.
       </p>
 
       <p>
-        Functions within the JavaScript runtime are also intercepted, so calls to functions like window.fetch are logged to console output.
-
-        Try it with the dog.ceo image API, click the image to get a new random dog image, and see the logged fetch calls in the Android app's console output.
+        Functions within the JavaScript runtime are also intercepted, so calls
+        to functions like window.fetch are logged to console output. Try it with
+        the dog.ceo image API, click the image to get a new random dog image,
+        and see the logged fetch calls in the Android app's console output.
       </p>
 
       <button @click="callAPI">
-        <img :src="resultAPI" alt="" srcset="">
+        <img :src="resultAPI" alt="" srcset="" />
       </button>
     </section>
   </main>
-
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted } from "vue";
 
 export default {
-  name: 'App',
+  name: "App",
   setup() {
-    const permissions = ref({})
-    const photo = ref(null)
-    const resultAPI = ref(null)
+    const permissions = ref({});
+    const photo = ref(null);
+    const resultAPI = ref(null);
 
     onMounted(() => {
       if (window.native) {
-        permissions.value = JSON.parse(window.native.getPermissionStatus())
+        permissions.value = JSON.parse(window.native.getPermissionStatus());
       }
 
       callAPI();
-    })
+    });
 
     const requestPermissions = () => {
       if (window.native) {
-        window.native.requestPermissions()
-        permissions.value = JSON.parse(window.native.getPermissionStatus())
+        window.native.requestPermissions();
+        permissions.value = JSON.parse(window.native.getPermissionStatus());
       }
-    }
+    };
 
     const takePhoto = () => {
       if (window.native) {
-        photo.value = window.native.takePhoto()
+        photo.value = window.native.takePhoto();
       }
-    }
+    };
 
     const callAPI = async () => {
       try {
-        const response = await window.fetch('https://dog.ceo/api/breeds/image/random')
-        const data = await response.json()
-        resultAPI.value = data.message
+        const response = await window.fetch(
+          "https://dog.ceo/api/breeds/image/random",
+        );
+        const data = await response.json();
+        resultAPI.value = data.message;
       } catch (error) {
-        console.error('Error fetching API:', error)
+        console.error("Error fetching API:", error);
       }
-    }
+    };
 
     return {
       window,
@@ -125,14 +147,13 @@ export default {
       resultAPI,
 
       requestPermissions,
-      takePhoto
-    }
-  }
-}
+      takePhoto,
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 main {
   margin: 0 auto;
   padding: 2em;
@@ -177,7 +198,8 @@ table {
   width: 50%;
 }
 
-th, td {
+th,
+td {
   border: 1px solid #333;
   padding: 0.5em;
   text-align: left;
@@ -186,6 +208,4 @@ th, td {
 td:nth-child(2) {
   text-align: center;
 }
-
-
 </style>
